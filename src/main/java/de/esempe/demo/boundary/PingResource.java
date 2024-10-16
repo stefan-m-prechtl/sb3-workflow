d3b57eff2af629bcd3ce9b6d44fd922abe90b27f
Stefan Prechtl <stfanmprechtl@web.de>
2024-10-16 19:11:44 +0200

    Neustart

---
+++ src/main/java/de/esempe/demo/boundary/PingResource.java
@@ -0,0 +1,30 @@
+package de.esempe.demo.boundary;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import de.esempe.demo.domain.PingResultRecord;
+
+@RestController
+@RequestMapping("ping")
+public class PingResource
+{
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<PingResultRecord> getPing()
+	{
+		final LocalDateTime now = LocalDateTime.now();
+		final var body = new PingResultRecord(now, "Ping vom Server");
+		final var status = HttpStatus.OK;
+
+		// final var result = ResponseEntity.ok(body);
+		final var result = ResponseEntity.status(status).body(body);
+		return result;
+	}
+
+}
---
+++ src/test/java/de/esempe/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.esempe.demo;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests
+{
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
