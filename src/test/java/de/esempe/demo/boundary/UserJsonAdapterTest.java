84c75f33c869a43f060d15beb6365c127be8e94d
Stefan Prechtl <stfanmprechtl@web.de>
2024-10-17 21:40:50 +0200

    Erweiterung..

--- src/test/java/de/esempe/demo/boundary/UserJsonAdapterTest.java
+++ src/test/java/de/esempe/demo/boundary/UserJsonAdapterTest.java
@@ -4,7 +4,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.IOException;
 
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
@@ -13,12 +17,17 @@ import org.springframework.boot.test.json.JsonContent;
 import de.esempe.demo.domain.User;
 
 @JsonTest
+@TestMethodOrder(OrderAnnotation.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class UserJsonAdapterTest
 {
 	@Autowired
 	private JacksonTester<User> jacksonTester;
 
+	private String json = "";
+
 	@Test
+	@Order(1)
 	void convertToJson() throws IOException
 	{
 		final User user = new User();
@@ -30,5 +39,15 @@ public class UserJsonAdapterTest
 
 		assertThat(jsonUser).contains("first");
 
+		this.json = jsonUser;
+
+	}
+
+	@Test()
+	@Order(2)
+	void convertFromJson() throws IOException
+	{
+		final User user = this.jacksonTester.parseObject(this.json);
+		assertThat(user.getFirstname()).isEqualTo("Stefan");
 	}
 }
