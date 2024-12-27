356bc99cf6fd08200380bcd9812cd78caf8bce1b
Stefan Prechtl <stfanmprechtl@web.de>
2024-12-27 13:44:20 +0100

    Bugfix SQL

--- workflow/src/main/docker/sqlinit/02_createTables.sql
+++ workflow/src/main/docker/sqlinit/02_createTables.sql
@@ -1,32 +1,35 @@
 CREATE TABLE IF NOT EXISTS restdemo.t_users (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(10),
+    id BIGSERIAL PRIMARY KEY,
+    username VARCHAR(10) NOT NULL,
     firstname VARCHAR(50),
     lastname VARCHAR(50)
 );
 
 
 CREATE TABLE IF NOT EXISTS restdemo.t_globalroles (
-    id SERIAL PRIMARY KEY,
-    rolename VARCHAR(25),
+    id BIGSERIAL PRIMARY KEY,
+    rolename VARCHAR(25) NOT NULL,
     description VARCHAR(50)
 );
 
 CREATE TABLE IF NOT EXISTS restdemo.t_workflowroles (
-    id SERIAL PRIMARY KEY,
-    workflowobj UUID, 
-    rolename VARCHAR(25),
+    id BIGSERIAL PRIMARY KEY,
+    workflowobj UUID NOT NULL, 
+    rolename VARCHAR(25) NOT NULL,
     description VARCHAR(50)
 );
 
 CREATE TABLE IF NOT EXISTS restdemo.t_users2globalroles (
-    userid SERIAL PRIMARY KEY,
-    roleid SERIAL PRIMARY KEY
+    userid BIGINT,
+    roleid BIGINT,
+    PRIMARY KEY (userid, roleid)
+
 );
 
 CREATE TABLE IF NOT EXISTS restdemo.t_users2workflowroles (
-    userid SERIAL PRIMARY KEY,
-    roleid SERIAL PRIMARY KEY
+    userid BIGINT NOT NULL,
+    roleid BIGINT NOT NULL,
+    PRIMARY KEY (userid, roleid)
 );
 
 
